6c81c89c06218516396b7485fb868cb5911a6552
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-13 22:46:21 +0900

    add DeleteObject
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akasha/service/DeleteObjectSupport.scala
+++ src/main/scala/akasha/service/DeleteObjectSupport.scala
@@ -1,5 +1,6 @@
 package akasha.service
 
+import akasha.patch.Commit
 import akasha.service.Error.Reportable
 import io.finch._
 import akasha.Server
@@ -19,7 +20,56 @@ trait DeleteObjectSupport {
                  callerId: String) extends Task[Output[Unit]] with Reportable {
       def resource = Resource.forObject(bucketName, keyName)
       def runOnce = {
-        Ok()
+        val bucket = tree.findBucket(bucketName) match {
+          case Some(a) => a
+          case None => failWith(Error.NoSuchBucket())
+        }
+        val key = bucket.findKey(keyName) match {
+          case Some(a) => a
+          case None => failWith(Error.NoSuchKey())
+        }
+        val versioningEnabled = Versioning.fromBytes(bucket.versioning.get.get.asData.readBytes).enabled
+
+        // x-amz-delete-marker
+        // Specifies whether the versioned object that was permanently deleted was (true) or was not (false) a delete marker.
+        // In a simple DELETE, this header indicates whether (true) or not (false) a delete marker was created.
+        //
+        // x-amz-version-id
+        // Returns the version ID of the delete marker created as a result of the DELETE operation.
+        // If you delete a specific object version, the value returned by this header is the version ID of the object version deleted.
+        if (versionId.isDefined) {
+          Ok()
+        } else {
+          // simple DELETE
+          val patch = Commit.retry(key.versions) { patch =>
+            val version = patch.asVersion
+            version.init
+            // default acl
+            Commit.retry(version.acl) { patch =>
+              val dataPatch = patch.asData
+              dataPatch.init
+              dataPatch.writeBytes(Acl.t(callerId, Seq(
+                Acl.Grant(
+                  Acl.ById(callerId),
+                  Acl.FullControl()
+                )
+              )).toBytes)
+            }
+            version.meta.asData.writeBytes(
+              Meta.t(
+                isVersioned = false,
+                isDeleteMarker = true,
+                eTag = "",
+                attrs = KVList.builder.build,
+                xattrs = KVList.builder.build
+              ).toBytes
+            )
+          }
+          Ok()
+            .withHeader("x-amz-request-id" -> requestId)
+            .withHeader("x-amz-delete-marker" -> "true")
+            .withHeader("x-amz-version-id" -> (if (versioningEnabled) { patch.name } else { "null" }))
+        }
       }
     }
   }
--- src/main/scala/akasha/service/Versioning.scala
+++ src/main/scala/akasha/service/Versioning.scala
@@ -6,8 +6,9 @@ import scala.pickling.binary._
 object Versioning {
   case class t(value: Int) {
     def toBytes: Array[Byte] = this.pickle.value
+    def enabled = value == ENABLED
   }
-  def read(bytes: Array[Byte]): t = BinaryPickle(bytes).unpickle[t]
+  def fromBytes(bytes: Array[Byte]): t = BinaryPickle(bytes).unpickle[t]
 
   val UNVERSIONED = 0
   val ENABLED = 1
