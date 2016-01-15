6fab7ebad146a7c7a0b560ba2526b182a95e063f
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-15 09:40:15 +0900

    add missing file
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ src/main/scala/akasha/service/HeadBucketSupport.scala
@@ -0,0 +1,27 @@
+package akasha.service
+
+import akasha.Server
+import akasha.service.Error.Reportable
+import io.finch._
+
+trait HeadBucketSupport {
+  self: Server =>
+  object HeadBucket {
+    val matcher = get(string ? RequestId.reader ? CallerId.reader).as[t]
+    val endpoint = matcher { a: t => a.run }
+    case class t(bucketName: String,
+                 requestId: String,
+                 callerId: String) extends Task[Output[Unit]] with Reportable {
+      def resource = Resource.forBucket(bucketName)
+      def runOnce = {
+        val bucket = tree.findBucket(bucketName) match {
+          case Some(a) => a
+          case None => failWith(Error.NoSuchBucket())
+        }
+        // TODO check acl
+        Ok()
+          .withHeader("x-amz-request-id" -> requestId)
+      }
+    }
+  }
+}
