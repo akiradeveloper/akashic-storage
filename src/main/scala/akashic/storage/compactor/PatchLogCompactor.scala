6a0a3a3fe76289ae208be7667be06bb73df36e60
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-19 11:00:00 +0900

    impl PatchLogCompactor
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/compactor/PatchLogCompactor.scala
+++ src/main/scala/akashic/storage/compactor/PatchLogCompactor.scala
@@ -1,10 +1,19 @@
 package akashic.storage.compactor
 
-import akashic.storage.patch.PatchLog
+import akashic.storage.patch.{Patch, PatchLog}
 import akashic.storage.Server
+import akashic.storage.files
 
 case class PatchLogCompactor(unwrap: PatchLog, server: Server) extends Compactable {
-  def compact = {
+  override def compact: Seq[Compactable] = {
+    val maxIdCommitted = unwrap.find match {
+      case Some(a) => a.name.toInt
+      case None => return Seq()
+    }
+    files.children(unwrap.root)
+      .map(Patch(_))
+      .filter(_.name.toInt < maxIdCommitted)
+      .foreach(a => dispose(a.root))
     Seq()
   }
 }
