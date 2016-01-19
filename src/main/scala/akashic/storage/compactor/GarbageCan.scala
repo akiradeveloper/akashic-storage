9ec8fc35cbc2d90b4f08eb33635f280e89ba0412
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-19 11:33:59 +0900

    impl GarbageCan
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/compactor/GarbageCan.scala
+++ src/main/scala/akashic/storage/compactor/GarbageCan.scala
@@ -1,8 +1,28 @@
 package akashic.storage.compactor
 
-import java.nio.file.{Files, Path}
+import java.nio.file.{Files, Path, FileAlreadyExistsException}
+import akashic.storage.{files, strings}
 
 case class GarbageCan(root: Path) {
   def add(path: Path) {
+    val newPath = root.resolve(strings.random(32))
+    try {
+      // no need to be atomic because if the trash remains in the tree
+      // next compaction has a chance to find it out.
+      Files.move(path, newPath)
+    } catch {
+      case e: FileAlreadyExistsException => add(path)
+      case e: Throwable => throw e
+    }
+  }
+
+  // TODO run in background and periodically
+  // (no need to purge trashes on shutdown)
+  def cleanup {
+    // FIXME may try to delete non-existent directory
+    // in case other node has deleted it.
+    files.children(root).foreach { dir =>
+      files.purgeDirectory(dir)
+    }
   }
 }
