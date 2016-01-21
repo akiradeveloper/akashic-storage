14eef51cb09fca5ae35731b0947d93ece4ac3a53
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-21 21:17:24 +0900

    add McTest (using minio/mc as a test tool)
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ src/test/scala/akashic/storage/McTest.scala
@@ -0,0 +1,97 @@
+package akashic.storage
+
+import java.nio.file.{Paths, Files}
+
+import akashic.storage.server
+import akashic.storage.admin.User
+import org.apache.commons.io.FileUtils
+import scala.xml.XML
+import scalaj.http.Http
+import scala.sys.process.Process
+
+class McTest extends ServerTestBase {
+  case class FixtureParam()
+  override protected def withFixture(test: OneArgTest) = {
+    test(FixtureParam())
+  }
+
+  val alias = "akashic-storage"
+  val confDir = "/tmp/akashic-storage-mc"
+
+  def mc(cmd: String) = {
+    val cmdline = s"mc --config-folder=${confDir} ${cmd}"
+    println(cmdline)
+    Process(cmdline)
+  }
+
+  override def beforeEach() {
+    super.beforeEach()
+
+    val url = s"http://${server.address}/admin/user"
+    val postRes = Http(url).method("POST").asString
+    assert(postRes.code === 200)
+    val newUser = User.fromXML(XML.loadString(postRes.body))
+    println(newUser)
+
+    val confDirPath = Paths.get(confDir)
+    if (!Files.exists(confDirPath)) {
+      Files.createDirectory(confDirPath)
+    }
+    FileUtils.cleanDirectory(confDirPath.toFile)
+
+    assert(mc(s"config host add ${server.address} ${newUser.accessKey} ${newUser.secretKey} S3v2").! === 0)
+    assert(mc(s"config alias add ${alias} http://${server.address}").! === 0)
+  }
+
+  test("add buckets") { _ =>
+    assert(mc(s"ls ${alias}").!!.split("\n").length === 1)
+    assert(mc(s"mb ${alias}/abc").! === 0)
+    assert(mc(s"ls ${alias}").!!.split("\n").length === 1)
+    assert(mc(s"mb ${alias}/DEF").! === 0)
+    assert(mc(s"ls ${alias}").!!.split("\n").length === 2)
+  }
+
+  // test("cp file and cat") { _ =>
+  //   (mc(s"mb ${alias}/abc") !) orFail
+  //   val f = getTestFile("test.txt")
+  //
+  //   // by dir dest
+  //   (mc(s"--quiet cp ${f.getAbsolutePath} ${alias}/abc") !) orFail
+  //
+  //   assert((mc(s"cat ${alias}/abc/test.txt") !!).trim === "We love Scala!")
+  //
+  //   // by explicit dest name
+  //   (mc(s"--quiet cp ${f.getAbsolutePath} ${alias}/abc/test2.txt") !) orFail
+  //
+  //   assert((mc(s"cat ${alias}/abc/test2.txt") !!).trim === "We love Scala!")
+  // }
+  //
+  // test("share (presigned get)") { _ =>
+  //   (mc(s"mb ${alias}/abc") !) orFail
+  //   val f = getTestFile("test.txt")
+  //   (mc(s"--quiet cp ${f.getAbsolutePath} ${alias}/abc") !) orFail
+  //
+  //   val httpCli = HttpClients.createDefault
+  //   val url: String = (mc(s"share ${alias}/abc/test.txt") !!).split("\n")(1).split("URL: ")(1)
+  //
+  //   val method = new HttpGet(url)
+  //   val contents: String = IOUtils.toString(httpCli.execute(method).getEntity.getContent).trim
+  //   assert(contents === "We love Scala!")
+  // }
+  //
+  // test("diff") { _ =>
+  //   val f = getTestFile("test.txt")
+  //
+  //   (mc(s"mb ${alias}/abc") !) orFail
+  //
+  //   (mc(s"--quiet cp ${f.getAbsolutePath} ${alias}/abc") !) orFail
+  //
+  //   (mc(s"mb ${alias}/def") !) orFail
+  //
+  //   (mc(s"--quiet cp ${f.getAbsolutePath} ${alias}/def") !) orFail
+  //
+  //   (mc(s"diff ${f.getAbsolutePath} ${alias}/abc/test.txt") !) orFail
+  //
+  //   (mc(s"diff ${alias}/abc/test.txt ${alias}/def/test.txt") !) orFail
+  // }
+}
