2799d259f6f1d05c001efba61e8a4f9d6304b426
Kia King Ishii <kia.king.08@gmail.com>
2016-01-23 00:17:06 +0900

    Refactor few .gitignore, trailing spaces and such
    - Add Vagrant and project specific ignore files to .gitignore.
    - Fix indention in script.
    - Remove trailing spaces and add blank line at end of file.

--- installer/install.sh
+++ installer/install.sh
@@ -1,8 +1,8 @@
 NAME=akashic-storage
 if [ ! -e $NAME.jar ]; then
-    echo "$NAME.jar not found. please run compile-jar.sh [NG]"
-    exit 1
-fi 
+  echo "$NAME.jar not found. please run compile-jar.sh [NG]"
+  exit 1
+fi
 mkdir -p /opt/$NAME
 mkdir -p /opt/$NAME/jar
 mkdir -p /opt/$NAME/etc
@@ -11,7 +11,7 @@ cp $NAME.jar /opt/$NAME/jar
 if [ ! -e /opt/$NAME/etc/conf ]; then
   cp default.conf /opt/$NAME/etc/conf
 else
-  echo "/opt/$NAME/etc/conf exists. skipped [OK]" 
+  echo "/opt/$NAME/etc/conf exists. skipped [OK]"
 fi
 cp $NAME /etc/init.d
 chmod +x /etc/init.d/$NAME
