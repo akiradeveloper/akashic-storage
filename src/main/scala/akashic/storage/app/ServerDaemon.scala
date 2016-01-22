3e29823848d56c377abc22a543143476ca13b629
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-22 20:16:00 +0900

    fix
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/app/ServerDaemon.scala
+++ src/main/scala/akashic/storage/app/ServerDaemon.scala
@@ -3,10 +3,11 @@ package akashic.storage.app
 import akashic.storage.{Server, ServerConfig, server}
 import com.typesafe.config.ConfigFactory
 import org.apache.commons.daemon.{DaemonContext, Daemon}
+import java.io.File
 
 class ServerDaemon extends Daemon {
   override def init(context: DaemonContext): Unit = {
-    val config = ServerConfig(ConfigFactory.load("/opt/akashic-storage/etc/conf"), init=false)
+    val config = ServerConfig(ConfigFactory.parseFile(new File("/opt/akashic-storage/etc/conf")), init=false)
     server = Server(config)
   }
   override def start(): Unit = {
