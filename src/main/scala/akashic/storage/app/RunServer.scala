56cb1fe1fc8015d5a356bc412a95de48a0bae8f7
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-23 09:28:52 +0900

    add users for s3-tests
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/app/RunServer.scala
+++ src/main/scala/akashic/storage/app/RunServer.scala
@@ -1,9 +1,14 @@
 package akashic.storage.app
 
 import akashic.storage._
+import akashic.storage.admin.TestUsers
 import com.typesafe.config.ConfigFactory
 
 object RunServer extends App {
   server = Server(ServerConfig(ConfigFactory.load, init=true))
   server.start
+
+  // for s3-tests
+  server.users.addUser(TestUsers.s3testsMain)
+  server.users.addUser(TestUsers.s3testsAlt)
 }
