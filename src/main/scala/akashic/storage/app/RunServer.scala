20a15f70fee94fe93bdec136509cbb89422100c8
Akira Hayakawa <ruby.wktk@gmail.com>
2016-01-23 14:51:45 +0900

    fix
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/app/RunServer.scala
+++ src/main/scala/akashic/storage/app/RunServer.scala
@@ -8,6 +8,9 @@ object RunServer extends App {
   server = Server(ServerConfig(ConfigFactory.load, init=true))
   server.start
 
+  // workaround
+  server.users.addUser(TestUsers.hoge)
+
   // for s3-tests
   server.users.addUser(TestUsers.s3testsMain)
   server.users.addUser(TestUsers.s3testsAlt)
