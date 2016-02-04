e49d55625461711af6ae7467ea2de1bc7dbddbf4
Akira Hayakawa <ruby.wktk@gmail.com>
2016-02-04 20:52:55 +0900

    [ci skip] improve setup-mc.py
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- installer/setup-mc.py
+++ installer/setup-mc.py
@@ -1,9 +1,15 @@
 from xml.etree.ElementTree import *
+import argparse
 import httplib
 import os
 
-address = "localhost:10946"
-conn = httplib.HTTPConnection("localhost:10946")
+parser = argparse.ArgumentParser(description="setup mc")
+parser.add_argument("--host", default="localhost", help="ip, hostname")
+parser.add_argument("--port", default="10946", help="port number")
+args = parser.parse_args()
+
+dest = "%s:%s" % (args.host, args.port)
+conn = httplib.HTTPConnection(dest)
 conn.request("POST", "/admin/user")
 response = conn.getresponse()
 data = response.read()
@@ -14,5 +20,5 @@ secretKey = elem.find("SecretKey").text
 print("AccessKey: %s" % accessKey)
 print("SecretKey: %s" % secretKey)
 
-os.system("mc config alias add akashic-storage http://%s" % address)
-os.system("mc config host add http://%s %s %s S3v2" % (address, accessKey, secretKey))
+os.system("mc config alias add akashic-storage http://%s" % dest)
+os.system("mc config host add http://%s %s %s S3v2" % (dest, accessKey, secretKey))
