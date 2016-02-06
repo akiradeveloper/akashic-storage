d8bb13adddd9d27a643ffb06cd384e036ec0db3a
Akira Hayakawa <ruby.wktk@gmail.com>
2016-02-06 09:06:37 +0900

    add PutBucket
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ src/main/scala/akashic/storage/service/PutBucket.scala
@@ -0,0 +1,46 @@
+package akashic.storage.service
+
+import java.nio.file.Files
+
+import akashic.storage.server
+import akashic.storage.patch._
+import akashic.storage.service.Error.Reportable
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+
+object PutBucket {
+  val route = (put & extractBucket & extractRequest).as(t)(_.run)
+
+  case class t(bucketName: String, req: HttpRequest) extends Task[Route] {
+    def name = "PUT Bucket"
+    def resource = Resource.forBucket(bucketName)
+    def runOnce = {
+      val dest = server.tree.bucketPath(bucketName)
+
+      if (Files.exists(dest))
+        failWith(Error.BucketAlreadyExists())
+
+      Commit.once(dest) { patch =>
+        val bucketPatch = patch.asBucket
+        bucketPatch.init
+
+        Commit.replaceData(bucketPatch.acl) { patch =>
+          val dataPatch = patch.asData
+          dataPatch.write(Acl.t(callerId, Seq(
+            Acl.Grant(
+              Acl.ById(callerId),
+              Acl.FullControl()
+            )
+          )).toBytes)
+        }
+
+        Commit.replaceData(bucketPatch.versioning) { patch =>
+          val dataPatch = patch.asData
+          dataPatch.write(Versioning.t(Versioning.UNVERSIONED).toBytes)
+        }
+      }
+      complete(HttpEntity.Empty)
+    }
+  }
+}
