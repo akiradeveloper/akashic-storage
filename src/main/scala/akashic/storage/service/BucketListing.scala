d120410474389f49bfb45e26f0f4775cdf0c7ccc
Akira Hayakawa <ruby.wktk@gmail.com>
2016-02-28 19:58:36 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/service/BucketListing.scala
+++ src/main/scala/akashic/storage/service/BucketListing.scala
@@ -11,32 +11,65 @@ object BucketListing {
   }
   case class Single[T <: Filterable](x: T) extends Container[T] {
     def get: T = x
+    def prefixBy(delimiter: String): String = {
+      val s = get.name
+      s.indexOf(delimiter) match {
+        case -1 => s
+        case i => s.slice(0, i) + delimiter
+      }
+    }
   }
-  case class Group[T <: Filterable](xs: Seq[T]) extends Container[T] {
+  case class Group[T <: Filterable](xs: Seq[Single[T]], prefix: String) extends Container[T] {
     require(xs.size > 0)
-    val get: T = xs.last
+    val get: T = xs.last.get
     val size: Int = xs.size
   }
 
-  case class t[T <: Filterable](value: Seq[Container[T]]) {
-    def filterByPrefix(prefix: Option[String]): this.type = {
-      this
+  implicit class Filtering[T <: Filterable](value: Seq[Single[T]]) {
+    def takesOnlyAfter(lastName: Option[String]) = {
+      lastName match {
+        case Some(a) => value.dropWhile(_.get.name < a)
+        case None => value
+      }
     }
 
-    def takesOnlyAfter(lastName: Option[String]): this.type = {
-      this
+    def filterByPrefix(prefix: Option[String]) = {
+      prefix match {
+        case Some(a) => value.filter(_.get.name.startsWith(a))
+        case None => value
+      }
     }
 
-    def dropWhile(fn: Container[T] => Boolean): this.type = {
-      this
+    def dropWhile(fn: Single[T] => Boolean) = {
+      value.dropWhile(fn)
     }
+  }
 
-    def groupByDelimiter(delimiter: Option[String]): this.type = {
-      this
+  implicit class Grouping[T <: Filterable](value: Seq[Single[T]]) {
+    def groupByDelimiter(delimiter: Option[String]): Seq[Container[T]] = {
+      delimiter match {
+        case Some(a) =>
+          val deli = encodeKeyName(a)
+          val newValue: Seq[Container[T]] =
+            value
+            .groupBy(_.prefixBy(deli)).toSeq // [prefix -> seq(contents)]
+            .sortBy(_._1) // sort by prefix
+            .map { case (prefix, members) =>
+              if (members.size > 1) {
+                Group(members, prefix.slice(0, prefix.size - deli.size))
+              } else {
+                members(0)
+              }
+            }
+          newValue
+        case None => value
+      }
     }
+  }
 
-    def byMaxLen(n: Int): (this.type, Boolean) = {
-      (this, false)
+  implicit class Truncation[T <: Filterable](value: Seq[Container[T]]) {
+    def byMaxLen(n: Int) = {
+      (value, false)
     }
   }
 }
