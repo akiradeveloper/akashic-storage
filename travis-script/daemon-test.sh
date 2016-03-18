eedf484c3c6a6c31e00ea669266ed42e754e73d6
Akira Hayakawa <ruby.wktk@gmail.com>
2016-03-18 19:39:22 +0900

    travis: fix daemon-test
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- travis-script/daemon-test.sh
+++ travis-script/daemon-test.sh
@@ -12,33 +12,35 @@ akashic-admin-config <<INP
 passwd
 INP
 
-userId = `akashic-storage-add`
+userId=`akashic-storage-add`
 echo "userId: $userId"
 
 # alias, hostname, port
 akashic-admin-setup-mc $userId <<INP
-akast
+aka
 
 10946
 INP
 
-echo "start"
+ls -l /usr/default/java
+
+echo "**** start ****"
 sudo service akashic-storage start
 sudo service akashic-storage status
 
 echo "akiradeveloper" > /tmp/t
-mc mb akashic-storage/myb
-mc cp /tmp/t akashic-storage/myb/myo
-mc ls akashic-storage/myb
+mc mb aka/myb
+mc cp /tmp/t aka/myb/myo
+mc ls aka/myb
 
-echo "stop"
+echo "**** stop ****"
 sudo service akashic-storage stop
 sudo service akashic-storage status
 
-echo "start"
+echo "**** start ****"
 sudo service akashic-storage start
 sudo service akashic-storage status
-mc ls akashic-storage
-mc ls akashic-storage/myb
-mc cat akashic-storage/myb/myo > t-down
+mc ls aka
+mc ls aka/myb
+mc cat aka/myb/myo > t-down
 diff t t-down
