2d8472bd17fc06b812410bcd3e2b22d0f2640d45
Akira Hayakawa <ruby.wktk@gmail.com>
2016-03-20 10:11:48 +0900

    try
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- travis-script/daemon-test.sh
+++ travis-script/daemon-test.sh
@@ -2,12 +2,13 @@ cd ../installer
 sh compile-jar.sh
 sudo sh install.sh
 
-sudo mkdir -p /tmp/akashic-storage-test
+DIR=/tmp/akashic-storage-test
+mkdir -p $DIR
 
 echo "**** start ****"
-sudo service akashic-storage start
+service akashic-storage start
 sleep 10
-sudo service akashic-storage status
+service akashic-storage status
 
 # hostname, port, passwd
 akashic-admin-config <<INP
@@ -32,24 +33,24 @@ echo "akiradeveloper" > file-up
 mc mb aka/myb
 mc cp file-up aka/myb/myo
 mc ls aka/myb
-tree /mnt/akashic-storage
+tree $DIR
 
 echo "**** stop ****"
-sudo service akashic-storage stop
+service akashic-storage stop
 sleep 10
-sudo service akashic-storage status
+service akashic-storage status
 
 echo "**** start ****"
-sudo service akashic-storage start
+service akashic-storage start
 sleep 10
-sudo service akashic-storage status
-tree /mnt/akashic-storage
+service akashic-storage status
+tree $DIR
 mc ls aka
 mc ls aka/myb
 mc cat aka/myb/myo > file-down
 diff file-up file-down
 
 echo -- error.log --
-sudo cat /var/log/akashic-storage/error.log
+cat /var/akashic-storage/log/error.log
 echo -- all.log --
-sudo cat /var/log/akashic-storage/all.log
+cat /var/akashic-storage/log/all.log
