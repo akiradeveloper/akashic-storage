ca94fbd339d37ea34d048603c798c6761171c5ff
Akira Hayakawa <ruby.wktk@gmail.com>
2016-03-22 08:27:37 +0900

    [skip ci] add CachingTest (but failing)
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/caching/CacheMap.scala
+++ src/main/scala/akashic/storage/caching/CacheMap.scala
@@ -6,11 +6,11 @@ trait CacheMap[K, V] {
 }
 
 object CacheMap {
-  class Null[K, V] extends CacheMap[K, V] {
+  case class Null[K, V]() extends CacheMap[K, V] {
     def find(k: K): Option[V] = None
     def insert(k: K, v: V) = {}
   }
-  class Guava[K, V](backing: com.google.common.cache.Cache[K, V]) extends CacheMap[K, V] {
+  case class Guava[K, V](backing: com.google.common.cache.Cache[K, V]) extends CacheMap[K, V] {
     override def find(k: K): Option[V] = backing.getIfPresent(k) match {
       case null => None
       case a => Some(a)
---
+++ src/test/scala/akashic/storage/CachingTest.scala
@@ -0,0 +1,43 @@
+package akashic.storage
+
+import java.nio.file.{Paths, Path}
+
+import akashic.storage.caching.{CacheMap, Cache}
+import com.google.common.cache.CacheBuilder
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+class CachingTest extends FunSuite with BeforeAndAfterEach {
+
+  val guava = new CacheMap.Guava[String, String](
+    CacheBuilder.newBuilder
+      .maximumSize(32)
+      .build())
+
+  var cache: Cache[String] = _
+
+  override def beforeEach: Unit = {
+    guava.backing.invalidateAll()
+
+    cache = new Cache[String] {
+      override val filePath: Path = Paths.get("/tmp/t")
+      override def writer: (String) => Array[Byte] = (a: String) => a.getBytes
+      override def reader: (Array[Byte]) => String = (a: Array[Byte]) => a.toString
+      override def cacheMap: CacheMap[K, String] = guava
+    }
+  }
+
+  test("simple get") {
+    cache.put("hoge")
+    val r0 = cache.get
+    assert(r0 === "hoge")
+    val r1 = cache.get
+    assert(r1 === "hoge")
+  }
+
+  test("invalidate and get") {
+    cache.put("hige")
+    assert(cache.get === "hige")
+    guava.backing.invalidateAll()
+    assert(cache.get === "hige")
+  }
+}
