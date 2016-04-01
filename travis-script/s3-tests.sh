7637ea9d9b18ee33354f8b0f7649385d70916676
Akira Hayakawa <ruby.wktk@gmail.com>
2016-04-01 23:45:19 +0900

    travis: moving creating log folders to each test
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- travis-script/s3-tests.sh
+++ travis-script/s3-tests.sh
@@ -1,6 +1,4 @@
+sudo mkdir -p /var/log/akashic-storage; sudo chmod o+rwx /var/log/akashic-storage
+
 cd ../s3-tests
 sh run.sh
-echo -- all.log --
-cat /var/log/akashic-storage/all.log
-echo -- error.log --
-cat /var/log/akashic-storage/error.log
