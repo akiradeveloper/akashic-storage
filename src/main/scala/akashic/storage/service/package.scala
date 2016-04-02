8979dcfce398c4192386dd387b0bf73f9e02a1af
Akira Hayakawa <ruby.wktk@gmail.com>
2016-04-02 19:33:21 +0900

    service: factor out Directives
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/akashic/storage/service/package.scala
+++ src/main/scala/akashic/storage/service/package.scala
@@ -1,23 +1,14 @@
 package akashic.storage
 
 import java.net.{URLDecoder, URLEncoder}
-import java.util.concurrent.TimeUnit
 
 import akashic.storage.admin._
-import akashic.storage.auth.GetCallerId
-import akashic.storage.service.Acl.Grant
-import akka.http.scaladsl.model.Multipart
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.directives.DebuggingDirectives
-import akka.http.scaladsl.server.{Directive0, Directive, Directive1}
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
-import scala.collection.immutable
-import scala.concurrent.Await
-import scala.concurrent.duration.{Duration, FiniteDuration}
-
-package object service {
+package object service extends Directives {
   // first appearance wins
   implicit class _Option[A](unwrap: Option[A]) {
     def `<+`(other: Option[A]): Option[A] = 
@@ -27,39 +18,6 @@ package object service {
       }
   }
 
-  def withParameter(name: String): Directive0 = parameter(name).tflatMap(a => pass)
-
-  private val extractGrantHeaders: Directive1[immutable.Seq[Acl.GrantHeader]] =
-    extractRequest.map(a => a.headers
-         .filter(_.name.startsWith("x-amz-grant-"))
-         .map(a => Acl.GrantHeader.parseLine(a.name, a.value))
-      )
-  val extractGrantsFromHeaders: Directive1[immutable.Seq[Grant]] =
-    extractGrantHeaders.map(_.map(_.makeGrants).flatten)
-
-  val optionalBinaryBody: Directive1[Option[Array[Byte]]] = entity(as[Array[Byte]]).map { a =>
-    if (a.size == 0) {
-      None
-    } else {
-      Some(a)
-    }
-  }
-
-  val optionalStringBody: Directive1[Option[String]] = entity(as[String]).map { a =>
-    a match {
-      case "" => None
-      case a => Some(a)
-    }
-  }
-
-  val extractMetadata: Directive1[HeaderList.t] =
-    extractRequest.map(a => a.headers
-         .filter(_.name.startsWith("x-amz-meta-"))
-         .map(a => (a.name, a.value))
-         .foldLeft(HeaderList.builder) { case (acc, (k, v)) => acc.append(k, v) }
-         .build
-      )
-
   val X_AMZ_REQUEST_ID = "x-amz-request-id"
   val X_AMZ_VERSION_ID = "x-amz-version-id"
   val X_AMZ_DELETE_MARKER = "x-amz-delete-marker"
@@ -68,10 +26,6 @@ package object service {
 
   def encodeKeyName(keyName: String): String = URLEncoder.encode(keyName, "UTF-8")
   def decodeKeyName(keyName: String): String = URLDecoder.decode(keyName, "UTF-8")
-  val extractBucket: Directive1[String] = path(Segment ~ (Slash | PathEnd))
-  val extractObject: Directive[(String, String)] = path(Segment / Rest).tmap {
-    case (bucketName: String, keyName: String) => (bucketName, encodeKeyName(keyName))
-  }
 
   trait AuthorizableTask extends Task with Error.Reportable with Authorizable with RequestIdAllocable with Measure
   type AuthorizedAPI = AuthorizableTask
@@ -81,10 +35,4 @@ package object service {
 
   val logger = Logger(LoggerFactory.getLogger("akashic.storage.service"))
   val apiLogger = withLog(logger).tflatMap(_ => DebuggingDirectives.logRequestResult(""))
-
-  def authorizeS3v2(resource: String, requestId: String): Directive1[String] =
-    extractRequest.map { req =>
-      val authKey = auth.authorize(resource, req)
-      GetCallerId(authKey, requestId, resource)()
-    }
 }
