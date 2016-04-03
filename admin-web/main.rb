220514dc34b10e89225e116e771deed663a52fef
Akira Hayakawa <ruby.wktk@gmail.com>
2016-04-03 12:39:30 +0900

    admin-web: embed keys
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- admin-web/main.rb
+++ admin-web/main.rb
@@ -4,8 +4,13 @@ set :port, 8080
 
 require "sinatra/reloader" if development?
 
+require "rexml/document"
+
 get "/" do
-  @access_key = "hoge"
-  @secret_key = "hige"
+  newUserId = `akashic-admin-add`
+  xml = `akashic-admin-get #{newUserId}`
+  doc = REXML::Document.new(xml)
+  @access_key = doc.elements["User/AccessKey"].text
+  @secret_key = doc.elements["User/SecretKey"].text
   erb :index
 end
